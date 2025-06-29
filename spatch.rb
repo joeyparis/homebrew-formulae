62bdc4a476c4c20b3e0e9751363d4198d5f5374d
Joey Paris <joey@leadjig.com>
2025-06-29 18:00:42 -0400

    Initial install script commit

---
+++ spatch.rb
@@ -0,0 +1,17 @@
+class SPatch < Formula
+  desc "Script to create a git branch comprised of patches for multiple branches"
+  homepage "https://github.com/joeyparis/spatch"
+  url "https://github.com/joeyparis/spatch/archive/v0.0.1.tar.gz"
+  sha256 "0fe8afd895a81b3e4e4a6ab55f1a75ae736e5388889580f7dfe5888570a71e6c"
+  version "0.0.1"
+
+  def install
+    bin.install "spatch.sh" => "spatch"
+  end
+
+  def caveats
+    <<~EOS
+      This is a very early release with no documentation yet available. Use at your own risk.
+    EOS
+  end
+end
